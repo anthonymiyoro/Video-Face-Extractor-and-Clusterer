d1336ceeaa2026a4a5c839c6031d20b324e301fe
anthonymiyoro <amiyoro@gmail.com>
2023-04-13 12:42:59 +0300

    Added test file and test video

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -1,9 +1,18 @@
 import os
+import sys
 import time
-import pytest
-import cv2
 
+# Get the absolute path to the top-level directory
+project_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(project_dir)
+required_module_dir = os.path.join(parent_dir, 'Video-Face-Extractor-and-Clusterer')
+
+# Add the top-level directory to Python module search path
+sys.path.insert(0, required_module_dir)
+
+# Import the collect_face function from the video_extract module
 from extract_faces_from_video import collect_faces
+
 
 def test_collect_faces():
     # Set up the test
@@ -14,7 +23,7 @@ def test_collect_faces():
 
     # Call the function and measure how long it takes
     start_time = time.time()
-    collect_faces(video_path, save_folder)
+    collect_faces(video_path, save_folder, 5)
     end_time = time.time()
 
     # Assert that the function produces at least 12 faces
@@ -22,7 +31,7 @@ def test_collect_faces():
     assert num_faces >= 12
 
     # Assert that the function takes no more than 15 seconds to run
-    assert end_time - start_time <= 104
+    assert end_time - start_time <= 106
 
     # Clean up the test
     for file_name in os.listdir(save_folder):
